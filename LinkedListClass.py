0369301c7eacba452070cc4c4ee43d645328d4c7
Дмитрий Волков <roskey@mail.ru>
2024-12-31 15:19:43 +0300

    Односвязанный список

--- LinkedListClass.py
+++ LinkedListClass.py
@@ -5,34 +5,42 @@ class Node:
 
 
 class LinkedList:
-
+    List = []
+    """Создание одно-связанного списка"""
     def __init__(self):
         self.head = None
 
     def insert_at_head(self, data):
+        """Вставляет данные в начало списка"""
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
         else:
             new_node.next_node = self.head
             self.head = new_node
+        LinkedList.List.insert(0, new_node.data)
         return f"Узел с данными {new_node.data} добавлен в начало списка"
 
     def insert_at_end(self, data):
+        """Вставляет данные в конец списка"""
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
+            LinkedList.List.append(new_node.data)
             return
         current_node = self.head
         while current_node.next_node:
             current_node = current_node.next_node
         current_node.next_node = new_node
+        LinkedList.List.append(new_node.data)
         return f"Узел с данными {new_node.data} добавлен в конец списка"
 
     def remove_node_position(self, rm_position):
+        """Удаляет данные списка по индексу. Start_index=1"""
         if rm_position == 1:
             removed_node = self.head
             self.head = self.head.next_node
+            LinkedList.List.pop(0)
             return f"Удален узел с данными {removed_node.data} позиции {rm_position}"
         current_node = self.head
         current_node_position = 1
@@ -43,14 +51,17 @@ class LinkedList:
             return "Ничего не удалено"
         removed_node = current_node.next_node
         current_node.next_node = current_node.next_node.next_node  # removed_node.next_node
+        LinkedList.List.pop(rm_position)
         return f"Удален узел с данными {removed_node.data} позиции {rm_position}"
 
     def insert_at_position(self, data, node_position):
+        """Добавляет данные в список по индексу. Start_index=1"""
         new_node = Node(data)
         if node_position == 1:
             self.insert_at_head(data)
             # new_node.next_node = self.head
             # self.head = new_node
+            LinkedList.List.append(new_node.data)
             return f"Узел с данными {new_node.data} добавлен на позицию {node_position}"
         """Опционально"""
         current_node = self.head
@@ -69,9 +80,11 @@ class LinkedList:
             return None
         new_node.next_node = current_node.next_node
         current_node.next_node = new_node
+        LinkedList.List.insert(node_position-1, new_node.data)
         return f"Узел с данными {new_node.data} добавлен на позицию {node_position}"
 
     def print_ll(self):
+        """Распечатывает список"""
         current_node = self.head
         while current_node:
             print(current_node.data)
@@ -79,14 +92,17 @@ class LinkedList:
         return "Данные списка выведены"
 
     def get(self, data):
+        """Если есть такие донные, то получает их.
+            Если таких данных нет - вернет False, None"""
         current_node = self.head
         while current_node:
             if current_node.data == data:
-                return True, current_node
+                return True, current_node        # Здесь Ture лишний, так как если есть current_node, то это и есть True
             current_node = current_node.next_node
         return False, None
 
     def change_data(self, node_data, change_data):
+        """Заменяет найденные донные на новые донные"""
         current_node = self.head
         current_node_position = 1
         while current_node:
@@ -99,7 +115,7 @@ class LinkedList:
 
     # def change_data(self, node_data, change_data):
     """Это второй способ для примера"""
-    #     result, current_node = self.get(node_data)
+    #     result, current_node = self.get(node_data)    # Комментарий в 91 строке
     #     if result:
     #         current_node.data = change_data
     #         return "Данные изменены!"
